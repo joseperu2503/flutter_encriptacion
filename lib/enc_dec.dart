75e84864b42aef7a91661813325584def1560c20
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2024-03-14 10:22:16 -0500

    caja

--- lib/enc_dec.dart
+++ lib/enc_dec.dart
@@ -1,55 +1,86 @@
+import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
-import 'package:convert/convert.dart';
-
 import 'package:crypto/crypto.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:flutter_encriptacion/encrypt_response.dart';
+import 'package:cryptography/cryptography.dart' as cryptography;
 
-const keyCry = '3420Key2022App@@';
+const keyCry = '123456';
 
-dynamic encryp(String text) {
+dynamic encryp(String text) async {
   Uint8List salt = _generateRandomBytes(8);
 
-  Uint8List salted = Uint8List(0);
-  Uint8List dx = Uint8List(0);
+  final pbkdf2 = cryptography.Pbkdf2(
+    macAlgorithm: cryptography.Hmac.sha256(),
+    iterations: 1000,
+    bits: 256,
+  );
 
-  while (salted.length < 48) {
-    dx = _md5(Uint8List.fromList([...dx, ...keyCry.codeUnits, ...salt]));
-    salted = Uint8List.fromList([...salted, ...dx]);
-  }
+  final newSecretKey = await pbkdf2.deriveKey(
+    secretKey: cryptography.SecretKey(keyCry.codeUnits),
+    nonce: salt,
+  );
 
-  Uint8List key = salted.sublist(0, 32);
-  Uint8List iv = salted.sublist(32, 48);
+  final key2 = await newSecretKey.extractBytes();
 
-  final encrypter = Encrypter(AES(Key(key), mode: AESMode.cbc));
-  final encrypted = encrypter.encrypt(text, iv: IV(iv));
+  final key = Key(Uint8List.fromList(key2));
+  final iv = IV.fromSecureRandom(16); // Generar un nuevo IV
+
+  final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+  final encrypted = encrypter.encrypt(text, iv: iv);
 
   return {
     "ct": encrypted.base64,
-    "iv": hex.encode(iv),
-    "s": hex.encode(salt),
+    "iv": iv.base64,
+    "s": base64.encode(salt),
   };
 }
 
-String decryp(EncrypResponse encrypResponse) {
-  Uint8List salt = Uint8List.fromList(hex.decode(encrypResponse.s));
-  Uint8List iv = Uint8List.fromList(hex.decode(encrypResponse.iv));
+// String decryp(EncrypResponse encrypResponse) {
+//   Uint8List salt = Uint8List.fromList(hex.decode(encrypResponse.s));
+//   Uint8List iv = Uint8List.fromList(hex.decode(encrypResponse.iv));
 
-  Uint8List concatedPassphrase =
-      Uint8List.fromList([...keyCry.codeUnits, ...salt]);
+//   Uint8List concatedPassphrase =
+//       Uint8List.fromList([...keyCry.codeUnits, ...salt]);
 
-  List<Uint8List> md5 = [];
-  md5.add(_md5(concatedPassphrase));
-  Uint8List result = md5[0];
-  for (int i = 1; i < 3; i++) {
-    md5.add(_md5(Uint8List.fromList([...md5[i - 1], ...concatedPassphrase])));
-    result = Uint8List.fromList([...result, ...md5[i]]);
-  }
+//   List<Uint8List> md5 = [];
+//   md5.add(_md5(concatedPassphrase));
+//   Uint8List result = md5[0];
+//   for (int i = 1; i < 3; i++) {
+//     md5.add(_md5(Uint8List.fromList([...md5[i - 1], ...concatedPassphrase])));
+//     result = Uint8List.fromList([...result, ...md5[i]]);
+//   }
 
-  final key = Key(result.sublist(0, 32));
+//   final key = Key(result.sublist(0, 32));
 
-  final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+//   final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+//   final encrypted = Encrypted.fromBase64(encrypResponse.ct);
+
+//   final decrypted = encrypter.decrypt(encrypted, iv: IV(iv));
+//   return decrypted;
+// }
+
+Future<String> decryp(EncrypResponse encrypResponse) async {
+  Uint8List salt = base64.decode(encrypResponse.s);
+  Uint8List iv = base64.decode(encrypResponse.iv);
+
+  final pbkdf2 = cryptography.Pbkdf2(
+    macAlgorithm: cryptography.Hmac.sha256(),
+    iterations: 1000, // 20k iterations
+    bits: 256, // 256 bits = 32 bytes output
+  );
+
+  final newSecretKey = await pbkdf2.deriveKey(
+    secretKey: cryptography.SecretKey(keyCry.codeUnits),
+    nonce: salt,
+  );
+
+  final key2 = await newSecretKey.extractBytes();
+
+  final key = Key(Uint8List.fromList(key2));
+
+  final encrypter = Encrypter(AES(key, mode: AESMode.cbc, padding: 'PKCS7'));
   final encrypted = Encrypted.fromBase64(encrypResponse.ct);
 
   final decrypted = encrypter.decrypt(encrypted, iv: IV(iv));
@@ -65,3 +96,63 @@ Uint8List _generateRandomBytes(int length) {
 Uint8List _md5(Uint8List data) {
   return Uint8List.fromList(md5.convert(data).bytes);
 }
+
+String encryptAES(String plainText, String password) {
+  String salt = _generateRandomBytes2(16);
+  final iv = IV.fromLength(16);
+  final key = Key.fromUtf8(salt);
+  final encrypter = Encrypter(AES(key));
+
+  final encrypted = encrypter.encrypt(plainText, iv: iv);
+  return encrypted.base64;
+}
+
+String decryptAES(String encryptedText, String password) {
+  String salt = _generateRandomBytes2(16);
+  final iv = IV.fromLength(16);
+  final key = Key.fromUtf8(salt);
+  final encrypter = Encrypter(AES(key));
+
+  final decrypted = encrypter.decrypt64(encryptedText, iv: iv);
+  return decrypted;
+}
+
+// String createCryptoRandomString([int length = 32]) {
+//   final values = List<int>.generate(length, (i) => pc.secureRandom.nextInt(256));
+//   return base64Url.encode(values);
+// }
+
+String _generateRandomBytes2(int length) {
+  var random = Random.secure();
+  var values = List<int>.generate(length, (index) => random.nextInt(256));
+  return base64UrlEncode(values);
+}
+
+// class EncryptionHandler {
+//   static const int keySize = 256;
+//   static const int derivationIterations = 1000;
+
+//   static String decrypt(String encryptedData, String passPhrase) {
+//     final parts = encryptedData.split(":");
+//     final salt = base64.decode(parts[0]);
+//     final iv = IV.fromBase64(parts[1]);
+//     final encryptedText = base64.decode(parts[2]);
+
+//     final pbkdf2 = cryptography.Pbkdf2(
+//       macAlgorithm: cryptography.Hmac.sha256(),
+//       iterations: 10000, // 20k iterations
+//       bits: 256, // 256 bits = 32 bytes output
+//     );
+
+//     final key = cryptography.Pbkdf2(
+//       iterations: derivationIterations,
+//       macAlgorithm: cryptography.Hmac.sha256(),
+//       keySize: keySize ~/ 8,
+//     ).process(utf8.encode(passPhrase), Uint8List.fromList(salt));
+
+//     final encrypter = Encrypter(AES(Key(key), mode: AESMode.cbc));
+//     final decrypted = encrypter.decrypt(Encrypted(encryptedText), iv: iv);
+
+//     return decrypted;
+//   }
+// }
--- lib/home.dart
+++ lib/home.dart
@@ -19,24 +19,24 @@ class _HomeState extends State<Home> {
       "password": "1234Nimda",
     };
 
-    final formEncryp = encryp(jsonEncode(form));
+    final formEncryp = await encryp(jsonEncode(form));
     print(formEncryp);
 
     //desencriptar
     final encrypResponse = EncrypResponse.fromJson(formEncryp);
-    final respuestaDesencriptada = decryp(encrypResponse);
+    final respuestaDesencriptada = await decryp(encrypResponse);
     print(respuestaDesencriptada);
   }
 
-  desencriptar() {
+  desencriptar() async {
     final encrypResponse = EncrypResponse.fromJson({
       "ct":
-          "rZGqr3o2bCEJKyIiglkp6eHV8cPj7cbM8fjqAu9m/29o00YA3ovYfWrRRHcAz9nOM9sYyLP9rUHA/28R2RH3jJ4MGXg7G7iGLkqdy9nlTeTYJRoUhndUubBCjoboavvRzgT3qQ26TRqJMKKlAI49SDRVRwWp3uyjlWO/027NPLmbYTyYzX3XIOT4c+IiVV7Ey81TGRj2PzDcuk4IEF6I/w8RCWA87aAzvLsvzRdac1/l0XL1TBW//20Qkr8rqkGEsliJfg5QaW5VfuED+oNKkHiGMtZb8oqwufSauO1VM7tL9NJRR/QwnlyHZji6HqKsZbHWetNZvMowL28uSQvmetbtBRS9R9jB5Z/PpULlz78JAw5awm075nUCuUtBYkFNa8LmROdF5ur8TjnrkyjsMTz9DgANIVJSHcgXpUXH2VFivQW/aA7WMbFQXGP8sDNhL3hQG6zgmb4BpjFG4uk5W4ZaCQI78PpSCscW+KG780Z2M6HGItfcU4Sru5CWJ7Ie4IMV8eJNNHw3najqGIexerKtDYaaBQd9OiFUneEiWDfPEB4pxoO/Oi/tPiIve+NvMgAi4hiPcGrEgmzKsnWoFNRsz+Cw5BgHqqpICW1NQ5F6jnMZWEW5/010XeG6zTXoOd20/I0XKt0lzqgFGWfWTs7gbBJZjGmLRyxqGzv++EKmfJzFQJxAkmfvoyEApLDFyHdTvsXFs6DZNx9QKGkM50mtu4LIgeQ57AGxyY13dOa+BjLK6vByLe4lMsEcNLN1ImxioAgy0y7YQwf9tCCXVMO301H8g1AwpRgE8TNnxGz66TptpkpQFrWdan/kJjSf8Cog3YhiYJkFD9KuS244GdOlG5A4mjM+RUVYB8cypRn/hHlbCEGtikPSc9amp/muxE1Y/13ND2JnIvNUn0rpkHDvNu0EqN3Vdy2QHyWZdsIt12wjG9EG8OaAczXDrKIl8LtV2RhWhrL8voLvbB19Gw==",
-      "iv": "efb3b240f380de537904233bee2c1a41",
-      "s": "7023a828919b28b0"
+          "8dJc8rV0VjVsjVGzR9zhz7i4stQy1VqgYC+GjvDJYFcHK50EEMIKUhv2NivWFn/TCp8/oS0uiqwba6f/dspqqN/x6/tr46SJVN7YlxtTiK1y1NL7MBeU/7bpYrepilmU",
+      "iv": "cm7MtTON2o5/cdylqvHl5g==",
+      "s": "t2pTtDgrRC/AOQ44JBtxoGq/g6ZnMmOdf7jiaBUqHHg="
     });
 
-    final respuestaDesencriptada = decryp(encrypResponse);
+    final respuestaDesencriptada = await decryp(encrypResponse);
     print(respuestaDesencriptada);
   }
 
